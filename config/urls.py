4effaf9ae46022acd2e6ea51884d7bbc888a04a7
Abdoul <bignet15@gmail.com>
2026-07-09 02:53:41 +0000

    Branchement des routes sessions dans config

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.http import JsonResponse
-from django.urls import path
+from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
@@ -18,6 +18,9 @@ def health_check(request):
 urlpatterns = [
     path("", health_check, name="health-check"),
     path("admin/", admin.site.urls),
+
+    # Module sessions
+    path("api/sessions/", include("sessions_app.urls")),
 
     # Auth JWT
     path("api/auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
