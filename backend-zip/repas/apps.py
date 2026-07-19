0cf3833081f150686914269361d746781a7d7d48
Abdoul <bignet15@gmail.com>
2026-07-19 13:54:54 +0000

    Retire les archives backend du dépôt

--- backend-zip/repas/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class RepasConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "repas"
-
-    def ready(self):
-        from . import signals  # noqa: F401
